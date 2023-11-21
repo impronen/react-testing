ec95ce775a1fbfa7d76ed1e5046df984ffcded47
antero <impronen@gmail.com>
2023-11-21 20:27:25 +0200

    more stuff

---
+++ src/CustomButton.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line react/prop-types
+const CustomButton = ({ onClick }) => {
+    return (
+      <button onClick={onClick}>Click me</button> 
+    );
+  };
+  
+  export default CustomButton;
